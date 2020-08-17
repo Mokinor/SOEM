f2b41af8e375479529a9af8559f5ffaeff6f33b3
Mokinor <sebyer84@gmail.com>
2020-08-17 15:02:51 +0200

    crash

--- test/appli/app.c
+++ test/appli/app.c
@@ -12,18 +12,33 @@
 
 #include "ethercat.h"
 #include <pcap.h>
+#include <Windows.h>
+#include <time.h>
 #include "misc.h" /* LoadNpcapDlls */
 
 #define EC_TIMEOUTMON 500
+#define stack64k (64 * 1024)
 
+long NSEC_PER_SEC = 1000000000i64;
+struct timeval tv, t1, t2;
+int dorun = 0;
+int deltat, tmax = 0;
+long toff;
+long long gl_delta;
+int DCdiff;
+int os;
+uint8 ob;
+uint16 ob2;
+uint8 *digout = 0;
 char IOmap[4096];
-OSAL_THREAD_HANDLE thread1;
+OSAL_THREAD_HANDLE thread1, thread2;
 int expectedWKC;
 boolean needlf;
 volatile int wkc;
 boolean inOP;
 uint8 currentgroup = 0;
 int32 outData,inData;
+int cycleTime;
 
 void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data);
 
@@ -82,6 +97,7 @@ void simpletest(char *ifname) //ifname name of interface
 
 			ec_configdc(); // config distributed clocks
 
+			ec_dcsync01(1,1,cycleTime,1,0);
 			printf("Slaves mapped, state to SAFE_OP.\n");
 
 			/* wait for all slaves to reach SAFE_OP state */
@@ -128,51 +144,51 @@ void simpletest(char *ifname) //ifname name of interface
 				ec_statecheck(0, EC_STATE_OPERATIONAL, 50000);
 			} while (chk-- && (ec_slave[0].state != EC_STATE_OPERATIONAL));
 
+			dorun = 1;
 
 			if (ec_slave[0].state == EC_STATE_OPERATIONAL)
 			{
 				printf("Operational state reached for all slaves.\n");
 				inOP = TRUE; //Bool for OP status
+				
 				/* cyclic loop */
-				int64_t startTime = ec_DCtime; //enreg temps au départ de la com (eviter les temps inutilisables)
+				long long startTime = ec_DCtime; //enreg temps au départ de la com (eviter les temps inutilisables)
 											   // reférence prise sur le temps affiché par les DCs
-				for (i = 1; i <= 10000; i++)
+				for (i = 1; i <= 20000; i++)
 				{	
 
 					
-					if(inPDO32(1) != inData)
+					/*if(inPDO32(1) != inData)
 					{
 						outData++;
-					}
+					}*/
+					//outData++;
 					inData = inPDO32(1);
 					outPDO32(1,outData);
-					ec_send_processdata();
-					wkc = ec_receive_processdata(EC_TIMEOUTRET);
 
-					if (wkc >= expectedWKC)
-					{
+               			printf("Processdata cycle %5d , Wck %3d, DCtime %12lld, dt %12lld, O:",
+                  		dorun, wkc , ec_DCtime, gl_delta);
 
-						printf("Processdata cycle %4d, WKC %d , O:", i, wkc);
-
-						for (j = oloop-1; j >= 0; j--)
+						for (j = ec_slave[0].Obytes; j >= 0; j--)
 						{
 							printf(" %2.2x", ec_slave[0].outputs[j]); //printing outputs ?
 						}
 
 						printf(" I:");
-						for (j = iloop-1; j >= 0; j--)
+						for (j = ec_slave[0].Ibytes; j >= 0; j--)
 						{
 							printf(" %2.2x", ec_slave[0].inputs[j]); //printing inputs ?
 						}
 						//printf(" T(ns):%" PRId64 "", ec_DCtime - startTime);
 						//printf(" Slave cycle time : %I32d ", ec_slave[1].DCcycle);
-						printf(" Cycle time estimate (ns) : %I64d\r", (ec_DCtime - startTime) / i);
+						printf(" Cycle time (ns): %I64d Com time %I64d", (ec_DCtime - startTime) / dorun, (ec_DCtime - startTime));
 						printf("\r");
 						needlf = TRUE;
 						fflush(stdout);
-					}
+					
 					osal_usleep(5000);
 				}
+				dorun = 0;
 				inOP = FALSE;
 			}
 			else
@@ -205,6 +221,88 @@ void simpletest(char *ifname) //ifname name of interface
 		printf("No socket connection on %s\nExecute as root\n", ifname);
 	}
 }
+
+/* add ns to timespec */
+void add_timespec(struct timespec *ts, long addtime)
+{
+   long  sec, nsec;
+
+   nsec = addtime % NSEC_PER_SEC;
+   sec = (addtime - nsec) / NSEC_PER_SEC;
+   ts->tv_sec += sec;
+   ts->tv_nsec += nsec;
+   if ( ts->tv_nsec > NSEC_PER_SEC )
+   {
+      nsec = ts->tv_nsec % NSEC_PER_SEC;
+      ts->tv_sec += (ts->tv_nsec - nsec) / NSEC_PER_SEC;
+      ts->tv_nsec = nsec;
+   }
+}
+
+/* PI calculation to get linux time synced to DC time */
+void ec_sync(long long reftime, long cycletime , long *offsettime)
+{
+   static long  integral = 0;
+   long  delta;
+   /* set linux sync point 50us later than DC sync, just as example */
+   delta = (reftime - 50000) % cycletime;
+   if(delta> (cycletime / 2)) { delta= delta - cycletime; }
+   if(delta>0){ integral++; }
+   if(delta<0){ integral--; }
+   *offsettime = -(delta / 100) - (integral / 20);
+   gl_delta = delta;
+}
+
+/*port of clock_gettime func on win*/
+int clock_gettime(int dummy, struct timespec *spec)     
+{  
+	__int64 wintime; 
+	GetSystemTimeAsFileTime((FILETIME*)&wintime);
+	wintime      -=116444736000000000i64;  //1jan1601 to 1jan1970
+	spec->tv_sec  =wintime / 10000000i64;           //seconds
+	spec->tv_nsec =wintime % 10000000i64 *100;      //nano-seconds
+	return 0;
+}
+
+/* RT EtherCAT thread */
+OSAL_THREAD_FUNC_RT ecatthread(void *ptr)
+{
+   	struct timespec   ts; 
+	//struct timespec tleft;
+   	int ht;
+	long cycletime;
+
+   	clock_gettime(0, &ts);
+   	ht = (ts.tv_nsec / 1000000) + 1; /* round to nearest ms */
+   	ts.tv_nsec = ht * 1000000;
+   	cycletime = *(int*)ptr * 1000; /* cycletime in ns */
+   	toff = 0;
+   	dorun = 0;
+   	ec_send_processdata();
+   	while(1)
+   	{
+      /* calculate next cycle start */
+      add_timespec(&ts, cycletime + toff);
+      /* wait to cycle start */
+      //clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, &tleft);
+      if (dorun>0)
+      {
+         wkc = ec_receive_processdata(EC_TIMEOUTRET);
+		outData++;
+         dorun++;
+         /* if we have some digital output, cycle */
+         if( digout ) *digout = (uint8) ((dorun / 16) & 0xff);
+
+         if (ec_slave[0].hasdc)
+         {
+            /* calulate toff to get linux time and DC synced */
+            ec_sync(ec_DCtime, cycletime, &toff);
+         }
+         ec_send_processdata();
+      }
+   }
+}
+
 
 /* OS abstracted thread to manage errors */
 
@@ -293,6 +391,10 @@ int main(int argc, char *argv[])
 	int i = 0;
 	// pcap_t *adhandle;
 	char errbuf[PCAP_ERRBUF_SIZE];
+	
+
+	dorun =0;
+	cycleTime = atoi(argv[1]);
 
 	/* Load Npcap and its functions. */
 	if (!LoadNpcapDlls())
@@ -343,8 +445,10 @@ int main(int argc, char *argv[])
 	printf("SOEM (Simple Open EtherCAT Master)\nTest app\n");
 
 	/* create thread to handle slave error handling in OP */
-	// pthread_create( &thread1, NULL, (void *) &ecatcheck, (void*) &ctime);
+	// pthread_create( &thread1, NULL, (void *) &ecatcheck, (void*) &cycleTime);
 	osal_thread_create(&thread1, 128000, &ecatcheck, (void *)&ctime);
+	/* create RT thread */
+    osal_thread_create_rt(&thread1, stack64k * 2, &ecatthread, (void*) &cycleTime);
 	/* start cyclic part */
 	simpletest(d->name);
 

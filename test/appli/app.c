a34ada6d1d613dee1f127bded77da02dbc328c53
Mokinor <sebyer84@gmail.com>
2020-10-06 11:03:47 +0200

    application done, 4 streams in .csv outputs

--- test/appli/app.c
+++ test/appli/app.c
@@ -17,13 +17,14 @@
 #include <pcap.h>
 #include <Windows.h>
 #include <time.h>
-#include "misc.h" 
+#include "misc.h"
 
 /*	DEFINES	*/
 #define EC_TIMEOUTMON 500
 #define stack64k (64 * 1024)
-#define print_num 100000 
-//#define PRINT_OUT 
+#define print_num 100000
+//#define PRINT_OUT
+//#define PRINT_PDO
 
 /*	Variable declarations	*/
 long NSEC_PER_SEC = 1000000000i64;
@@ -46,14 +47,17 @@ boolean inOP;
 uint8 currentgroup = 0;
 int32 outData,inData;
 int cycleTime;
-int64 stream1[500000];
-int64 stream2[500000];
+int64 stream1[print_num];
+int64 stream2[print_num];
+int64 stream3[print_num];
+int64 stream4[print_num];
 int64 t1,t2;
 int64 real_cycle;
 int64 prev_real_cycle;
 int64 jitt;
 uint8 txbuf[128];
 uint8 rxbuf[128];
+int nbOfFilesOut = 0;
 
 
 /* pcap packet handler declaration*/
@@ -63,28 +67,29 @@ void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_cha
 /* .csv file output function takes filename and length of tables as params
 * can output 2 sets of data in current form
 */
-int output_csv(char *fname, int length)
+int output_csv(int length)
 {
    FILE *fp;
-
-   int  i;
-
-   fp = fopen(fname, "w");
-   if(fp == NULL)
-      return 0;
-   for (i = 0; i < length; i++)
-   {
-      fprintf(fp, "%d; %lld; %lld;\n", i, stream1[i], stream2[i]);
-   }
-   fclose(fp);
-
-   return 1;
+	printf("Writing file %d\n",nbOfFilesOut);
+    int  i;
+    char buffer[50];
+	sprintf(buffer, ".\\Outputs\\testOut%d.csv",nbOfFilesOut);
+	fp = fopen(buffer, "w");
+	if(fp == NULL)
+	return 0;
+    for (i = 0; i < length; i++)
+    {
+		fprintf(fp, "%d; %lld; %lld; %lld; %lld\n", i, stream1[i], stream2[i], stream3[i],stream4[i]);
+    }
+    fclose(fp);
+	//nbOfFilesOut++;
+    return 1;
 }
 
 
 /*!
 concatenation of slave inputs to an int for better printing and processing
-\param slave slave number 
+\param slave slave number
 */
 int32 inPDO32(int slave)
 {
@@ -97,9 +102,9 @@ int32 inPDO32(int slave)
 		slaveOffset = (slave-1)*4;
 	}
 	b0 = ec_slave[0].inputs[3 + slaveOffset];
-    b1 = ec_slave[0].inputs[2 + slaveOffset];
-    b2 = ec_slave[0].inputs[1 + slaveOffset];
-    b3 = ec_slave[0].inputs[0 + slaveOffset];
+	b1 = ec_slave[0].inputs[2 + slaveOffset];
+	b2 = ec_slave[0].inputs[1 + slaveOffset];
+	b3 = ec_slave[0].inputs[0 + slaveOffset];
 
 	res = b0 | b1 | b2 | b3 ;
 	return res;
@@ -109,14 +114,15 @@ int32 inPDO32(int slave)
 void outPDO32(int slave, uint32_t data)
 {
 	int slaveOffset = 0;
+	int nextSlaveOffset =0;
 	/* calculate offest in IOmap based on slave number */
 	if(slave > 1)
 	{
-		slaveOffset = (slave-1)*4;
+		slaveOffset = (slave-1) * (ec_slave[1].Obits/4);
 	}
 	for (int i = 0 + slaveOffset; i < 3 + slaveOffset ;i++)
 	{
-		ec_slave[0].outputs[i]= (uint8_t)(outData >> (8*i));
+		ec_slave[0].outputs[i]= (uint8_t)((data*slave) >> (8*i));
 	}
 }
 
@@ -136,7 +142,7 @@ int mbxhandler(int slave, uint8_t *mbxDataIn, uint8_t *mbxDataOut)
 		return 0;
 	}
 	/*Ccheck if value is changed*/
-	
+
 	/*Clear mbx buffer*/
 	ec_clearmbx(&mbxIn);
 
@@ -205,28 +211,28 @@ OSAL_THREAD_FUNC mailbox_reader(void *lpParam)
    * Used with a special bound impelmentaion of SOES. Will
    * trigger a fragmented transfer back of the same frame.
    */
-  	makeHeader( txbuf, sizeof(txbuf) - 6, 1);
-    mbxWkc = ecx_mbxsend(context, 1, (ec_mbxbuft *) txbuf, EC_TIMEOUTRXM );
+	  makeHeader( txbuf, sizeof(txbuf) - 6, 1);
+	mbxWkc = ecx_mbxsend(context, 1, (ec_mbxbuft *) txbuf, EC_TIMEOUTRXM );
 	if(mbxWkc < 1)
 	{
 		printf("Error mbxWkc = %d\n", mbxWkc);
 	}
-	
 
-	
+
+
    for (;;)
    {
-      /* Read mailbox if no other mailbox conversation is ongoing  eg. SDOwrite/SDOwrite etc.*/
-	 
-      mbxWkc = ecx_mbxreceive(context, 1, (ec_mbxbuft *)&rxbuf, EC_TIMEOUTRXM);
+	  /* Read mailbox if no other mailbox conversation is ongoing  eg. SDOwrite/SDOwrite etc.*/
+
+	  mbxWkc = ecx_mbxreceive(context, 1, (ec_mbxbuft *)&rxbuf, EC_TIMEOUTRXM);
 	  printf("Wkc : %d", mbxWkc);
 	  for (int j = 0;  j < sizeof(rxbuf) ; j++)
 	  {
 		  printf("MbxIn[%d] = %x \n",j,rxbuf[j]);
 	  }
-	  
+
 	  mbxWkc = ecx_mbxsend(context, 1, (ec_mbxbuft *) txbuf, EC_TIMEOUTRXM );
-      osal_usleep(10000 * 1000);
+	  osal_usleep(10000 * 1000);
 	  //mbxWkc = ecx_mbxsend(context, 1, (ec_mbxbuft *) txbuf, EC_TIMEOUTRXM );
  }
 }
@@ -253,26 +259,26 @@ void simpletest(char *ifname) //ifname name of interface
 		{
 
 			printf("%d slaves found and configured.\n", ec_slavecount);
-			
+
 			ec_configdc(); // config distributed clocks
-			
+
 			for(i = 1; i<= ec_slavecount; i++)
 			{
 				printf("dcsync%d\n",i);
 				ec_dcsync01(i,TRUE,cycleTime*1000,cycleTime*1000,0);
 			// 	ec_dcsync0(i,TRUE,cycleTime,0);
 			}
-			
+
 			ec_config_map(&IOmap); //configuration of the IO Map of devices
 
-			
 
-			
+
+
 			printf("Slaves mapped, state to SAFE_OP.\n");
 
 			/* wait for all slaves to reach SAFE_OP state */
 			ec_statecheck(0, EC_STATE_SAFE_OP, EC_TIMEOUTSTATE * 4);
-			
+
 			/*launch mailbox handler thread*/
 			//osal_thread_create(&thread3, 128000, &mailbox_reader, &ecx_context);
 
@@ -286,7 +292,7 @@ void simpletest(char *ifname) //ifname name of interface
 			ec_slave[0].state = EC_STATE_OPERATIONAL;
 			/* send one valid process data to make outputs in slaves happy*/
 			ec_send_processdata();								 // sending processdata makes slaves go from preop to safeop
-			int TimeOut = ec_receive_processdata(EC_TIMEOUTRET); // 
+			int TimeOut = ec_receive_processdata(EC_TIMEOUTRET); //
 			/* request OP state for all slaves */
 			ec_writestate(0);
 			chk = 2000;
@@ -307,47 +313,42 @@ void simpletest(char *ifname) //ifname name of interface
 			{
 				printf("Operational state reached for all slaves.\n");
 				inOP = TRUE; //Bool for OP status
-				
+
 				/* cyclic loop */
 				long long startTime = ec_DCtime; // approx. start time of cyclic communication (not used anymore)
 
-				printf("Communication in progress ...\n");					   
+				printf("Communication in progress ...\n");
 				// Can calculate communication time ~= print_num * 2ms
 				// for (i = 1; i <= print_num; i++)
 				while(1)
-				{	
+				{
 
-					// change output on input change
-					if(inPDO32(1) != inData)
-					{
-						outData++;
-					}
+
 
 					// refresh input value for next loop
-					inData = inPDO32(1);
+
 
 
 					#ifdef PRINT_OUT
-						int j;
-               			printf("Processdata cycle %5d , Wck %2d, DCtime %12lld, ct %9lld, Jitter %9lld, O:",
-                  		dorun, globalWkc , ec_DCtime, real_cycle,jitt);
 
-						/*jitter calculation*/
-						jitt = real_cycle - prev_real_cycle;
-						prev_real_cycle = real_cycle;
+						   printf("Processdata cycle %5d , Wck %2d, DCtime %12lld, ct %9lld, O:",
+						  dorun, globalWkc , ec_DCtime, real_cycle);
 
-						/* printing master outputs*/
-						for (j = ec_slave[0].Obytes-1; j >= 0; j--)
-						{
-							printf(" %2.2x", ec_slave[0].outputs[j]); 
-						}
-						
-						/*printing master inputs*/
-						printf(" I:");
-						for (j = ec_slave[0].Ibytes-1; j >= 0; j--)
-						{
-							printf(" %2.2x", ec_slave[0].inputs[j]); 
-						}
+						#ifdef PRINT_PDO
+							int j;
+							/* printing master outputs*/
+							for (j = ec_slave[0].Obytes-1; j >= 0; j--)
+							{
+								printf(" %2.2x", ec_slave[0].outputs[j]);
+							}
+
+							/*printing master inputs*/
+							printf(" I:");
+							for (j = ec_slave[0].Ibytes-1; j >= 0; j--)
+							{
+								printf(" %2.2x", ec_slave[0].inputs[j]);
+							}
+						#endif
 						//printf(" T(ns):%" PRId64 "", ec_DCtime - startTime);
 						//printf(" Slave cycle time : %I32d ", ec_slave[1].DCcycle);
 						//printf(" Cycle time (ns): %I64d Com time %I64d", (ec_DCtime - startTime) / dorun, (ec_DCtime - startTime));
@@ -362,7 +363,7 @@ void simpletest(char *ifname) //ifname name of interface
 				inOP = FALSE;
 			}
 			else
-			{	
+			{
 				/*return error if slaves not in OP*/
 				printf("Not all slaves reached operational state.\n");
 				ec_readstate();
@@ -387,7 +388,7 @@ void simpletest(char *ifname) //ifname name of interface
 		/* stop SOEM, close socket */
 		ec_close();
 		/* file output*/
-		output_csv("outputs.csv",print_num);
+
 	}
 	else
 	{
@@ -406,9 +407,9 @@ void add_timespec(struct timespec *ts, long addtime)
    ts->tv_nsec += nsec;
    if ( ts->tv_nsec > NSEC_PER_SEC )
    {
-      nsec = ts->tv_nsec % NSEC_PER_SEC;
-      ts->tv_sec += (ts->tv_nsec - nsec) / NSEC_PER_SEC;
-      ts->tv_nsec = nsec;
+	  nsec = ts->tv_nsec % NSEC_PER_SEC;
+	  ts->tv_sec += (ts->tv_nsec - nsec) / NSEC_PER_SEC;
+	  ts->tv_nsec = nsec;
    }
 }
 
@@ -427,49 +428,49 @@ void ec_sync(long long reftime, long cycletime , long *offsettime)
 }
 
 /* not used port of nanosleep function from Linux*/
-static void  nanosleep (struct timespec *requested_delay)  
-{  
-      time_t seconds = requested_delay->tv_sec; // second part   
-      long int nanoSeconds = requested_delay->tv_nsec; // nano seconds part  
-  if (seconds > 0) 
+static void  nanosleep (struct timespec *requested_delay)
+{
+	  time_t seconds = requested_delay->tv_sec; // second part
+	  long int nanoSeconds = requested_delay->tv_nsec; // nano seconds part
+  if (seconds > 0)
   {
 	DWORD msec = (DWORD) seconds * 1000 + nanoSeconds /  1000000i64;
-  	Sleep (msec); //If more than one second  
+	  Sleep (msec); //If more than one second
   }
-  else  
-   {     
-    static double frequency; // ticks per second  
-    if (frequency == 0)  
-     {  
-      LARGE_INTEGER freq;  
-      if (!QueryPerformanceFrequency (&freq))  
-       {  
-        /* Cannot use QueryPerformanceCounter. */
-        Sleep (nanoSeconds / 1000000);  
-        return;  
-       }  
-      frequency = (double) freq.QuadPart / 1000000000.0;     // ticks per nanosecond  
-     }  
-    double counter_difference = nanoSeconds * frequency;  
-    int sleep_part = (int) nanoSeconds / 1000000 - 10;  
-    LARGE_INTEGER start;  
-    QueryPerformanceCounter (&start);  
-    long long expected_counter = start.QuadPart + (long) counter_difference;  
-    if (sleep_part > 0)     // for milliseconds part  
-     Sleep(sleep_part);  
-	 LARGE_INTEGER stop;  
-    do                         // for nanoseconds part  
-     {  
-      QueryPerformanceCounter (&stop);  
-      
-	
-     }  while (!(stop.QuadPart >= expected_counter));
-   }  
- } 
+  else
+   {
+	static double frequency; // ticks per second
+	if (frequency == 0)
+	 {
+	  LARGE_INTEGER freq;
+	  if (!QueryPerformanceFrequency (&freq))
+	   {
+		/* Cannot use QueryPerformanceCounter. */
+		Sleep (nanoSeconds / 1000000);
+		return;
+	   }
+	  frequency = (double) freq.QuadPart / 1000000000.0;     // ticks per nanosecond
+	 }
+	double counter_difference = nanoSeconds * frequency;
+	int sleep_part = (int) nanoSeconds / 1000000 - 10;
+	LARGE_INTEGER start;
+	QueryPerformanceCounter (&start);
+	long long expected_counter = start.QuadPart + (long) counter_difference;
+	if (sleep_part > 0)     // for milliseconds part
+	 Sleep(sleep_part);
+	 LARGE_INTEGER stop;
+	do                         // for nanoseconds part
+	 {
+	  QueryPerformanceCounter (&stop);
+
+
+	 }  while (!(stop.QuadPart >= expected_counter));
+   }
+ }
 
 /*port of clock_gettime func on win*/
-int clock_gettime( struct timespec *spec)     
-{  
+int clock_gettime( struct timespec *spec)
+{
 	spec->tv_nsec = osal_current_time().usec * 1000;
 	return 0;
 }
@@ -477,58 +478,72 @@ int clock_gettime( struct timespec *spec)
 /* RT EtherCAT thread */
 OSAL_THREAD_FUNC_RT ecatthread(void *ptr)
 {
-   	
-	struct timespec   ts; 
+
+	struct timespec   ts;
 	//struct timespec tleft;
-   	int ht;
+	   int ht;
 	long cycletime;
-   	clock_gettime(&ts);
-   	ht = (ts.tv_nsec / 1000000) + 1; /* round to nearest ms */
-   	ts.tv_nsec = ht * 1000000;
-   	cycletime = *(int*)ptr * 1000; /* cycletime in ns */
-   	toff = 0;
-   	dorun = 0;
-   	ec_send_processdata();
-   	while(1)
-   	{
-      /* calculate next cycle start */
-      // add_timespec(&ts, cycletime + toff);
-      /* wait to cycle start */
+	   clock_gettime(&ts);
+	   ht = (ts.tv_nsec / 1000000) + 1; /* round to nearest ms */
+	   ts.tv_nsec = ht * 1000000;
+	   cycletime = *(int*)ptr * 1000; /* cycletime in ns */
+	   toff = 0;
+	   dorun = 0;
+	   ec_send_processdata();
+	   while(1)
+	   {
+	  /* calculate next cycle start */
+	  // add_timespec(&ts, cycletime + toff);
+	  /* wait to cycle start */
 	 //printf("nanosecond wait : %d\n",cycletime + toff);
-     //   do
+	 //   do
 	 //   {
 	 // 	  clock_gettime(&tleft);
 	 //   }
 	 //   while(!(tleft.tv_nsec >= ts.tv_nsec));
 
 	  osal_usleep((cycletime + toff)/ 1000);
-      if (dorun>0)
-      {
-         globalWkc = ec_receive_processdata(EC_TIMEOUTRET);
-		 
+	  if (dorun>0)
+	  {
+		 globalWkc = ec_receive_processdata(EC_TIMEOUTRET);
+
 		t1 = ec_DCtime;
 		real_cycle = t1-t2;
 		t2 = ec_DCtime;
-		 if(dorun < sizeof(stream1))
+		inData = inPDO32(1);
+		 if(dorun < (print_num + nbOfFilesOut*print_num))
 		 {
-			stream1[dorun] = real_cycle;
-			stream2[dorun] = toff;
+			stream1[dorun-(print_num*nbOfFilesOut)] = real_cycle;
+			stream2[dorun-(print_num*nbOfFilesOut)] = toff;
+			stream3[dorun-(print_num*nbOfFilesOut)] = inData;
+			stream4[dorun-(print_num*nbOfFilesOut)] = ec_DCtime;
+		 } 
+		 else
+		 {
+			while(!output_csv(print_num))
+			{
+				printf("Output\n");
+			}
+			nbOfFilesOut++;
 		 }
 
-         dorun++;
-         /* if we have some digital output, cycle */
-         //if( digout ) *digout = (uint8) ((dorun / 16) & 0xff);
-		outPDO32(1,outData);
-		outPDO32(2,outData);
-		//outPDO32(3,outData);
-		//outPDO32(4,outData);
-         if (ec_slave[0].hasdc)
-         {
-            /* calulate toff to get linux time and DC synced */
-            ec_sync(ec_DCtime, cycletime, &toff);
-         }
-         ec_send_processdata();
-      }
+
+
+		 dorun++;
+		 /* if we have some digital output, cycle */
+		 //if( digout ) *digout = (uint8) ((dorun / 16) & 0xff);
+		 outData = inData;
+		 for(int i = 1;i<ec_slavecount;i++)
+		 {
+			outPDO32(i,outData);
+		 }
+		 if (ec_slave[0].hasdc)
+		 {
+			/* calulate toff to get linux time and DC synced */
+			ec_sync(ec_DCtime, cycletime, &toff);
+		 }
+		 ec_send_processdata();
+	  }
    }
 }
 
@@ -622,7 +637,7 @@ int main(int argc, char *argv[])
 	int i = 0;
 	// pcap_t *adhandle;
 	char errbuf[PCAP_ERRBUF_SIZE];
-	
+
 	if (argc != 2 || atoi(argv[1]) < 100)
 	{
 		printf("Usage : app.exe [cycletime]\n Cycletime in microseconds >1000");
@@ -684,7 +699,7 @@ int main(int argc, char *argv[])
 	// pthread_create( &thread1, NULL, (void *) &ecatcheck, (void*) &cycleTime);
 	osal_thread_create(&thread1, 128000, &ecatcheck, (void *)&ctime);
 	/* create RT thread */
-    osal_thread_create_rt(&thread2, stack64k * 2, &ecatthread, (void*) &cycleTime);
+	osal_thread_create_rt(&thread2, stack64k * 2, &ecatthread, (void*) &cycleTime);
 	/* start cyclic part */
 	simpletest(d->name);
 
